ad7783ae1341d3a3a3f654c9f4420cb44076408e
Filippo Fontana <filippo@embrio.tech>
2022-10-07 11:57:23 +0200

    feat: add data model for currency balances

--- src/mappings/handlers/ormlTokensHandlers.ts
+++ src/mappings/handlers/ormlTokensHandlers.ts
@@ -11,50 +11,52 @@ export const handleTokenTransfer = errorHandler(_handleTokenTransfer)
 async function _handleTokenTransfer(event: SubstrateEvent<TokensTransferEvent>): Promise<void> {
   const [currency, from, to, amount] = event.event.data
 
+  // TRANCHE TOKEN TRANSFERS
   if (currency.isTranche) {
     const [poolId, trancheId] = currency.asTranche
     const fromAddress = String.fromCharCode(...from.toU8a())
     const toAddress = String.fromCharCode(...to.toU8a())
 
-    // FILTER OUT ALL TRANSACTIONS CONTAINING A POOL ADDRESS
-    if (fromAddress.startsWith('pool') || toAddress.startsWith('pool')) return
+    if (fromAddress.startsWith('pool') || toAddress.startsWith('pool')) {
+      logger.info(
+        `Tranche Token transfer tor tranche: ${poolId.toString()}-${trancheId.toString()}. ` +
+          `from: ${from.toString()} to: ${to.toString()} amount: ${amount.toString()} ` +
+          `at block ${event.block.block.header.number.toString()}`
+      )
 
-    logger.info(
-      `Token transfer tor tranche: ${poolId.toString()}-${trancheId.toString()}.
-      from: ${from.toString()} to: ${to.toString()} amount: ${amount.toString()}
-      at block ${event.block.block.header.number.toString()}`
-    )
+      // Get corresponding pool
+      const pool = await PoolService.getById(poolId.toString())
+      const tranche = await TrancheService.getById(poolId.toString(), trancheId.toHex())
+      const [fromAccount, toAccount] = await Promise.all([
+        AccountService.getOrInit(fromAddress),
+        AccountService.getOrInit(toAddress),
+      ])
 
-    // Get corresponding pool
-    const pool = await PoolService.getById(poolId.toString())
-    const tranche = await TrancheService.getById(poolId.toString(), trancheId.toHex())
-    const [fromAccount, toAccount] = await Promise.all([
-      AccountService.getOrInit(fromAddress),
-      AccountService.getOrInit(toAddress),
-    ])
+      // Update tranche price
+      await tranche.updatePriceFromRpc()
+      await tranche.save()
 
-    // Update tranche price
-    await tranche.updatePriceFromRpc()
-    await tranche.save()
+      const orderData = {
+        poolId: poolId.toString(),
+        trancheId: trancheId.toString(),
+        epochNumber: pool.pool.currentEpoch,
+        hash: event.extrinsic.extrinsic.hash.toString(),
+        timestamp: event.block.timestamp,
+        digits: (await CurrencyService.getById(pool.pool.currencyId)).currency.decimals,
+        price: tranche.trancheState.price,
+        amount: amount.toBigInt(),
+      }
 
-    const orderData = {
-      poolId: poolId.toString(),
-      trancheId: trancheId.toString(),
-      epochNumber: pool.pool.currentEpoch,
-      hash: event.extrinsic.extrinsic.hash.toString(),
-      timestamp: event.block.timestamp,
-      digits: (await CurrencyService.getById(pool.pool.currencyId)).currency.decimals,
-      price: tranche.trancheState.price,
-      amount: amount.toBigInt(),
+      // CREATE 2 TRANSFERS FOR FROM AND TO ADDRESS
+      // with from create TRANSFER_OUT
+      const txOut = InvestorTransactionService.transferOut({ ...orderData, address: fromAccount.account.id })
+      await txOut.save()
+
+      // with to create TRANSFER_IN
+      const txIn = InvestorTransactionService.transferIn({ ...orderData, address: toAccount.account.id })
+      await txIn.save()
     }
-
-    // CREATE 2 TRANSFERS FOR FROM AND TO ADDRESS
-    // with from create TRANSFER_OUT
-    const txOut = InvestorTransactionService.transferOut({ ...orderData, address: fromAccount.account.id })
-    await txOut.save()
-
-    // with to create TRANSFER_IN
-    const txIn = InvestorTransactionService.transferIn({ ...orderData, address: toAccount.account.id })
-    await txIn.save()
+  } else if (!currency.isTranche && !currency.isNone && !currency.isEmpty) {
+    //DO DOMETHING
   }
 }
