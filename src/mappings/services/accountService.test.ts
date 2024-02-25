c6cb3c7436a5abd33f0a70031cd72af9432277ed
Filippo Fontana <filippo@embriotech.ch>
2024-02-25 20:45:53 +0100

    test: fix according to new logic

--- src/mappings/services/accountService.test.ts
+++ src/mappings/services/accountService.test.ts
@@ -6,10 +6,9 @@ api.query['evmChainId'] = { chainId: jest.fn(() => ({ toString: () => '2030' }))
 
 test('Account is created in database', async () => {
   const id = 'ABCDE'
-  const chainId = '2030'
   const account = await AccountService.init(id)
   await account.save()
 
   expect(logger.info).toHaveBeenCalled()
-  expect(store.set).toHaveBeenCalledWith('Account', id, { id, chainId })
+  expect(store.set).toHaveBeenCalledWith('Account', id, { id, chainId: '0' })
 })
